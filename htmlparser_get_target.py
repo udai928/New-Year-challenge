67c1f87140e14a9054699604445bffdaa468cea8
Kuzuya Yudai <kuzuya_yudai@microad.co.jp>
2015-12-26 17:42:45 +0900

    お気に入りリストから商品名と値段を取る

--- htmlparser_get_target.py
+++ htmlparser_get_target.py
@@ -5,12 +5,21 @@ from bs4 import BeautifulSoup
 import urllib.request
 
 # 
-url = 'http://finance.yahoo.co.jp/'
+url = 'http://www.amazon.co.jp/gp/registry/wishlist/BCSQ72LZSFIU/ref=topnav_lists_1'
 req = urllib.request.Request(url)
 response = urllib.request.urlopen(req)
 html = response.read()
 soup = BeautifulSoup(html, "lxml")
 # you must add '_' after 'class'
-nikkei_url = soup.find('dt',class_="title floatL")
+#favorite_elements = soup.find_all('h5')
 
-print(nikkei_url)
+favorite_elements = soup.find_all("div",class_="a-text-left a-fixed-left-grid-col a-col-right")
+
+for favorite_element in favorite_elements:
+    print(favorite_element.a.string)
+    favorite_price_elements = favorite_element.find_all("div",class_="a-section price-section")
+    for favorite_price_element in favorite_price_elements:
+        favorite_price_span = favorite_price_element.find('span')
+        print(favorite_price_span.text)
+
+    print("#######################################")
